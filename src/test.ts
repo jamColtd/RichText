7435336a5529beff5c441568e66330c6a3f106c1
dm <1664546116@qq.com>
2021-07-04 17:33:23 +0800

    :test 获取鼠标选中的文字

---
+++ src/test.ts
@@ -0,0 +1,28 @@
+// interface Font{
+//     fontBold:string;
+//     fontItalics:string;
+// }
+
+// var font = {
+//     fontBold:'fontBold',
+//     fontItalics:'fontItalics'
+// }
+$(function(){
+    $('#p').html('TypeScrip 的视频我们就暂时告一段落，紧接着会更新 Vue3 的课程，希望下伙伴们继续跟我一期学习。')
+    // 获取鼠标选中的文字
+    $('p').mouseup(function(e) {
+        let txt:any,text:string;
+        txt = window.getSelection()
+        text = txt.toString();
+        return text
+    })
+})
+
+function Font(font:string){
+    if(font === 'fontBold'){
+        console.log("B")
+    }else if(font === 'fontItalics'){
+        console.log("I")
+    }
+}
+
